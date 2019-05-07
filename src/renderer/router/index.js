df7be4f1cd8e42ed90cf596452fc992a496749c3
unknown <3010383334@qq.com>
2019-05-07 22:08:54 +0800

    ...

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -16,35 +16,35 @@ export default new Router({
       component: require('@/components/window/mainWindow').default,
       children:[
         {
-          path: '/window/overview',
+          path: 'overview',
           name: 'overview',
           component: require('@/components/views/overview/overview').default
         },
         {
-          path: '/window/codingRules',
+          path: 'codingRules',
           name: 'codingRules',
           component: require('@/components/views/codingRules/codingRules').default
         },
         {
-          path: '/window/taskManagement',
+          path: 'taskManagement',
           name: 'taskManagement',
           component: 
           require('@/components/views/taskManagement/taskManagement').default
         },
         {
-          path: '/window/processManagement',
+          path: 'processManagement',
           name: 'processManagement',
           component: 
           require('@/components/views/processManagement/processManagement').default
         },
         {
-          path: '/window/templateManagement',
+          path: 'templateManagement',
           name: 'templateManagement',
           component: 
             require('@/components/views/templateManagement/templateManagement').default,
           children: [
             {
-              path: '/window/templateManagement/createTemplate',
+              path: 'createTemplate',
               name: 'createTemplate',
               component:
               require('@/components/views/templateManagement/createTemplate').default
@@ -52,7 +52,7 @@ export default new Router({
           ]
         },
         {
-          path: '/window/dictionaryManagement',
+          path: 'dictionaryManagement',
           name: 'dictionaryManagement',
           component: 
           require('@/components/views/dictionaryManagement/dictionaryManagement').default

447f9e952a4b1435c5d731b4629ce642ad1935bf
unknown <3010383334@qq.com>
2019-05-08 17:37:02 +0800

    完成协作概况页面的开发

--- src/renderer/components/views/overview/overview.vue
+++ src/renderer/components/views/overview/overview.vue
@@ -4,10 +4,18 @@
       ref="orderStatistics" 
       :options="orderStatisticsOptions">
     </HighCharts>
+
+    <Table 
+      stripe 
+      size="small" 
+      :columns="columns" 
+      :data="tableData">
+    </Table>
   </div>
 </template>
 
 <script>
+import expandRow from './table-expand.vue'
 import HighCharts from '@/components/public/highCharts'
 export default {
   name: 'overview',
@@ -44,11 +52,107 @@ export default {
             text:'创建工单数'
           }
         },
-        series:[{
-          name:'工单数',
-          data:[2, 3, 5, 4, 6, 3],
-        }]
+        series:[
+          {
+            name:'工单数',
+            data:[2, 3, 5, 4, 6, 3],
+          },{
+            type: 'spline',
+            name: '平均值',
+            data: [3, 2.67, 3, 6.33, 3.33,4],
+            zones:[{
+              color: '#f7a35c',
+            }],
+            marker: {
+              lineWidth: 2,
+              lineColor: '#f7a35c',
+              fillColor: '#7cb5ec' 
+            }
+          }
+        ]
       },
+      columns:[
+        {
+          type: 'expand',
+          width: 30,
+          render: (h, params) => {
+            return h(expandRow, {
+              props: {
+                row: params.row.sub
+              }
+            })
+          }
+        },
+        {
+          title: '流程',
+          key: 'process',
+        },
+        {
+          title: '用户名',
+          key: 'userName',
+        },
+        {
+          title: '创建工单数',
+          key: 'create',
+          width:'150',
+          align:'center',
+          sortable: true
+        },
+        {
+          title: '待办',
+          key: 'need',
+          align:'center',
+          sortable: true
+        },
+        {
+          title: '已办',
+          key: 'create_time',
+          align:'center',
+          sortable: true
+        },
+        {
+          title: '驳回',
+          key: 'create_time1',
+          align:'center',
+          sortable: true
+        },
+        {
+          title: '时间',
+          key: 'time',
+          sortable: true
+        },
+      ],
+      tableData:[
+        {
+          process:'测试',
+          userName:'admin',
+          create:1,
+          need:2,
+          create_time:3,
+          create_time1:4,
+          time:'xxxx-xx-xx',
+          sub:[
+            {
+              process:'测试1',
+              userName:'admin1',
+              create:1,
+              need:1,
+              create_time:13,
+              create_time1:2,
+              time:'xxxx-xx-xx1',
+            }
+          ]
+        },
+        {
+          process:'测试',
+          userName:'admin',
+          create:32,
+          need:231,
+          create_time:321,
+          create_time1:442,
+          time:'xxxx-xx-xx',
+        }
+      ]
     }
   },
   created() {
@@ -63,12 +167,18 @@ export default {
     },
   },
   components:{
-    HighCharts
+    HighCharts,
+    expandRow
   }
 }
 </script>
 
 <style lang='less' scoped>
   @import "../../../common/less/color.less";
-  
+  .ivu-table-wrapper {
+    margin-top: 20px;
+  }
+  .hide {
+    display: none;
+  }
 </style>
---
+++ src/renderer/components/views/overview/table-expand.vue
@@ -0,0 +1,92 @@
+<template>
+  <Table 
+    stripe 
+    size="small" 
+    class="expand-table"
+    :showHeader="false" 
+    :columns="columns" 
+    :data="row"> 
+  </Table>
+</template > 
+</template>
+
+<script>
+  export default {
+    props: ['row'],
+    data() {
+      return {
+        columns:[
+          {
+            type: 'expand',
+            width: 30,
+            render: (h, params) => {
+              if(params.row.sub && params.row.sub.length) {
+                this.columns.type = ''
+                this.columns.width = 0
+              }
+              return h(expandRow, {
+                props: {
+                  row: params.row.sub
+                }
+              })
+            }
+          },
+          {
+            title: '流程',
+            key: 'process',
+          },
+          {
+            title: '用户名',
+            key: 'userName',
+          },
+          {
+            title: '创建工单数',
+            key: 'create',
+            width:'150',
+            align:'center',
+            filters: [],
+            filterMultiple: false,
+            filterMethod (value, row) {
+
+            }
+          },
+          {
+            title: '待办',
+            key: 'need',
+            align:'center',
+            sortable: true
+          },
+          {
+            title: '已办',
+            key: 'create_time',
+            align:'center',
+            sortable: true
+          },
+          {
+            title: '驳回',
+            key: 'create_time1',
+            align:'center',
+            sortable: true
+          },
+          {
+            title: '时间',
+            key: 'time',
+            sortable: true
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+  .expand-row {
+    margin-bottom: 16px;
+  }
+  td.ivu-table-expanded-cell {	
+    padding:0 !important;	
+  }
+  .expand-table .ivu-icon-ios-arrow-forward:before {
+    display: none;
+  }
+</style>

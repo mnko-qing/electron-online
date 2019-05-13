2f848cdd44f46ba5b59d03cdbaaca2d5571723c9
unknown <3010383334@qq.com>
2019-05-13 23:13:23 +0800

    ‘添加用户和设置抽屉

--- src/renderer/components/sideBarMenu/settings.vue
+++ src/renderer/components/sideBarMenu/settings.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="settings">
-    settings oho
+    <header>设置</header>
+    <div class="main">
+      <article>
+        <label>文件导出路径设置</label>
+        <div class="export-route">C:\Users\30103\Desktop</div>
+      </article>
+      <Divider />
+      <article>
+        <label>字体大小设置</label>
+        <Slider
+          :min="14"
+          :max="24" 
+          show-input
+          v-model="fontSize">
+        </Slider>
+      </article>
+    </div>
   </div>
 </template>
 
@@ -10,7 +26,7 @@ export default {
   name:'settings',
   data() {
     return {
-      
+      fontSize:14,
     }
   },
   mounted() {
@@ -21,7 +37,29 @@ export default {
 
 <style lang="less" scoped>
   .settings {
-
+    header,footer {
+      height: 40px;
+      line-height: 40px;
+      padding-left: 30px;
+    }
+    header {
+      border-bottom: 1px solid #e8eaec;
+    }
+    .main {
+      article {
+        padding: 30px;
+      }
+      .ivu-divider-horizontal {
+        margin: 0;
+      }
+      .export-route {
+        height: 30px;
+        line-height: 30px;
+        border:1px solid #e8eaec;
+        margin-top: 10px;
+        padding-left: 10px;
+      }
+    }
   }
 </style>
 
--- src/renderer/components/sideBarMenu/user.vue
+++ src/renderer/components/sideBarMenu/user.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="user">
     <header>账号</header>
+    <div class="main">
+      <Icon size="50" type="ios-contact"/>
+      <span>用户名</span>
+      <Collapse simple>
+        <Panel name="1" hide-arrow>
+          <Icon size="16" type="ios-information-circle-outline" />
+          <span>关于在线协作</span>
+          <Icon size="16" class="right-arrow" type="ios-arrow-forward" />
+          <p slot="content">
+            在线协作吧啦吧啦吧啦吧啦一大堆！
+          </p>
+        </Panel>
+    </Collapse>
+    </div>
     <footer class="no-text-select" @click="exit">
       <Icon type="ios-power-outline" size="18"/>
       <span class="pl-10">注销</span>
@@ -42,6 +56,26 @@ export default {
     }
     header {
       border-bottom: 1px solid #e8eaec;
+    }
+    .main {
+      padding-top: 20px;
+      &>i:first-child {
+        color: #d1cf94;
+        padding-right: 20px;
+        padding-left: 30px;
+      }
+      .ivu-collapse-simple {
+        margin-top: 20px;
+        .ivu-collapse-header {
+          &>i:first-child {
+            margin-right: 0;
+          }
+          .right-arrow {
+            float: right;
+            line-height: 38px;
+          }
+        }
+      }
     }
     footer {
       position: absolute;
